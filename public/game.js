f5260d0cf3583def7b5a00cc897e78e5070857b8
anrid <anridig@gmail.com>
2016-10-16 19:01:53 +0700

    Music!

--- public/game.js
+++ public/game.js
@@ -1,4 +1,4 @@
-/* globals Image */
+/* globals Image, XMLHttpRequest */
 'use strict'
 
 const SPRITE_BLOCK_SIZE = 32
@@ -11,33 +11,62 @@ const BLOCK_SIZE = SPRITE_BLOCK_SIZE / 4
 const STARTING_GAME_SPEED = 1 / 4
 const MAX_RUNTIME = 60000
 
+const IMAGES = [
+  { id: 'gameSprites', url: '/tetris.png' }
+]
+const SOUNDS = [
+  // { id: 'bgMusic1', url: '/sounds/dk-main.mp3' },
+  // { id: 'bgMusic2', url: '/sounds/dk-start.mp3' },
+  // { id: 'bgMusic3', url: '/sounds/dk-howhigh.mp3' },
+  // { id: 'bgMusic4', url: '/sounds/dk-hammer.mp3' }
+  { id: 'bgMusic1', url: '/sounds/bg-music-01.mp3' },
+  { id: 'bgMusic2', url: '/sounds/bg-music-02.mp3' },
+  { id: 'bgMusic3', url: '/sounds/bg-music-03.mp3' },
+  { id: 'bgMusic4', url: '/sounds/bg-music-04.mp3' }
+  // { id: 'bgMusic5', url: '/sounds/bg-music-05.mp3' }
+]
+
 const pieces = createPieces()
 const tetraminos = { 'LB': 2, 'LL': 4, 'LR': 4, 'CU': 1, 'SR': 2, 'TR': 4, 'SL': 2 }
-// NOTE: Simply force a set of tetraminos during testing.
+
+// NOTE: Force a set of tetraminos when testing.
 // const tetraminos = { 'LB': 2 }
 
 const ctx = document.getElementById('game').getContext('2d')
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
 // ctx.globalCompositeOperation = 'destination-over'
 
-runGame()
+// Run game on load.
+window.onload = runGame
 
 function runGame () {
-  console.log('Tetris is running.')
+  console.log('Loading Tetris ..')
   loadResources()
   .then(resources => {
+    console.log('Tetris loaded.')
+    // FIXME: Remove this later.
+    dumpPieces(resources.images[0].image)
+
+    // Create a new game.
     const game = createNewGame(resources)
+
+    // Hookup keyboard events.
     enableKeyboardEvents(game)
+
+    // Start the game loop.
     window.requestAnimationFrame(() => runGameLoop(game))
   })
 }
 
 function runGameLoop (game) {
+  // Let’s do this.
   moveAndRotatePiece(game)
   moveEverything(game)
   drawPieces(game)
+
   game.stats.runtime = Date.now() - game.stats.runningSince
   if (game.stats.runtime > MAX_RUNTIME) {
-    console.log('Game engine stopped, stats:', game.stats)
+    stopGame(game)
   } else {
     window.requestAnimationFrame(() => runGameLoop(game))
   }
@@ -55,10 +84,6 @@ function moveEverything (game) {
   // No rows remaining ? Time for a new piece.
   if (rowsRemaining <= 0) {
     // Don’t allow the current piece to ever go past the bottom.
-    // console.log(
-    //   'No rows remaining:',
-    //   'board.rows=', game.board.rows, 'piece.rows=', data.rows, 'current.row=', game.current.row
-    // )
     game.current.row += rowsRemaining
     game.all.push(game.current)
     game.current = getPieceFromBag(game)
@@ -71,7 +96,17 @@ function createNewGame (resources) {
   const bag = getRandomBagOfTetraminos().slice(0, 5)
 
   const game = {
-    sprites: resources.sprites,
+    sprites: resources.images[0].image,
+    sounds: resources.sounds,
+    soundsPlaying: { },
+    // Shuffled background music playlist.
+    musicPlaylist: shuffle([
+      'bgMusic1',
+      'bgMusic2',
+      'bgMusic3',
+      'bgMusic4'
+    ]),
+    musicPlaylistIndex: 0,
     board: {
       x: BOARD_WIDTH,
       y: 0,
@@ -92,14 +127,41 @@ function createNewGame (resources) {
     stats: {
       runningSince: Date.now(),
       runtime: 0
-    }
+    },
+    running: true
   }
-
   // Rock our first piece !
   game.current = getPieceFromBag(game)
 
-  console.log('New game:', game)
+  // Start the music.
+  playBackgroundMusic(game)
+
+  console.log('Created new game:', game)
   return game
+}
+
+function stopGame (game) {
+  game.running = false
+  // Kill all sounds.
+  Object.keys(game.soundsPlaying)
+  .forEach(x => game.soundsPlaying[x].stop())
+  console.log('Game stopped. Stats:', game.stats)
+}
+
+function playBackgroundMusic (game) {
+  const id = game.musicPlaylist[game.musicPlaylistIndex]
+  console.log('Playing background music:', id)
+
+  playSoundInGame(id, game)
+  game.soundsPlaying[id].onended = () => {
+    console.log('Background music ended.')
+    if (game.running) {
+      delete game.soundsPlaying[id]
+      playBackgroundMusic(game)
+    }
+  }
+  game.musicPlaylistIndex++
+  game.musicPlaylistIndex = game.musicPlaylistIndex % game.musicPlaylist.length
 }
 
 function getPieceFromBag (game) {
@@ -202,19 +264,18 @@ function getRandomBagOfTetraminos () {
 }
 
 function loadResources () {
-  const sprites = new Image()
-  const p = new Promise(resolve => {
-    console.log('Loading game sprites ..')
-    sprites.onload = () => {
-      dumpPieces(sprites)
-      console.log('Game sprites loaded successfully.')
-      resolve({
-        sprites
-      })
-    }
-  })
-  sprites.src = '/tetris.png'
-  return p
+  const promises = []
+
+  // Load sprites.
+  IMAGES.forEach(x => promises.push(loadImage(x.id, x.url)))
+
+  // Load all sounds.
+  SOUNDS.forEach(x => promises.push(loadSoundBuffer(x.id, x.url)))
+
+  return Promise.all(promises).then(res => ({
+    images: [res[0]],
+    sounds: res.slice(1)
+  }))
 }
 
 function enableKeyboardEvents (game) {
@@ -317,4 +378,61 @@ function createPieces () {
     SL1: { x: 104, y: SZ * 6 + 8, w: 24, h: 16 },
     SL2: { x: 104 + SZ, y: SZ * 6, w: 16, h: 24 }
   }
+}
+
+function loadImage (id, url) {
+  return new Promise(resolve => {
+    const img = new Image()
+    console.log('Loading image:', url)
+    img.onload = () => resolve(img)
+    img.src = url
+  })
+  .then(image => {
+    console.log(`Loaded image: ${url} (${id}) successfully.`)
+    return { id, url, image }
+  })
+}
+
+function playSoundInGame (id, game) {
+  const sound = game.sounds.find(x => x.id === id)
+  if (sound) {
+    const source = audioCtx.createBufferSource()
+    source.buffer = sound.buffer
+    source.connect(audioCtx.destination)
+    source.start(0)
+    game.soundsPlaying[id] = source
+  }
+}
+
+function loadSoundBuffer (id, url) {
+  return new Promise((resolve, reject) => {
+    // Load buffer asynchronously
+    const request = new XMLHttpRequest()
+    request.open('GET', url, true)
+    request.responseType = 'arraybuffer'
+    request.onload = function () {
+      // Asynchronously decode the audio file data in request.response
+      audioCtx.decodeAudioData(
+        request.response,
+        function (buffer) {
+          if (!buffer) {
+            return reject(new Error('error decoding file data: ' + url))
+          }
+          resolve(buffer)
+        },
+        function (error) {
+          reject(new Error('decode audio error: ' + error))
+        }
+      )
+    }
+    request.onerror = function (error) {
+      reject(new Error('load sound buffer XHR error:', error))
+    }
+    console.log('Loading sound:', url)
+    request.send()
+  })
+  .then(buffer => {
+    console.log(`Loaded sound: ${url} (${id}) successfully.`)
+    return { id, url, buffer }
+  })
 }
